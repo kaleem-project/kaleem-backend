9b59d37137c1b57628785b3cdf1f4c6843295e9e
0xGhazy <hossam192216@fci.bu.edu.eg>
2023-05-25 23:07:26 +0300

    Adding update endpoint from issue no #9

--- main.py
+++ main.py
@@ -33,15 +33,18 @@ def auth() -> Response:
         username, password = body["username"], body["password"]
         db_obj = DataabseAPI(DB_URI, DB_NAME)
         # search for this username in db
-        result = db_obj.getRecords({"username": username, "password": password}, "Accounts")
+        result = db_obj.getRecords(
+            {"username": username, "password": password}, "Accounts")
         if len(result) == 1:
-            user_map = {"id": str(result[0]["_id"]), "email": str(result[0]["email"]), "topic": "login"}
+            user_map = {"id": str(result[0]["_id"]), "email": str(
+                result[0]["email"]), "topic": "login"}
             token = generate_jwt(SECRET_KEY, TIME_WINDOW, user_map)
             return jsonify({"data": {"token": token}, "code": 200})
         else:
             return jsonify({"message": "<username> or <password> is incorrect", "code": 400})
     else:
         return jsonify({"message": "<username> or <password> key is missing", "code": 400})
+
 
 @app.route("/api/v1/reset/<user_token>", methods=["GET"])
 def reset_password(user_token) -> Response | tuple[Response, int] | str:
@@ -51,11 +54,13 @@ def reset_password(user_token) -> Response | tuple[Response, int] | str:
         new_password = body["new_password"]
         user_id, topic = result["user_id"], result["topic"]
         if topic == "reset-password" and user_id != "":
-            result = database_obj.updateRecord(ObjectId(user_id),{"password": new_password},"Accounts")
+            result = database_obj.updateRecord(
+                ObjectId(user_id), {"password": new_password}, "Accounts")
             return jsonify({"message": "Password updated successfully", "code": 200})
     except:
         return jsonify({"message": "Invalid authentication token", "code": 400}), 400
     return ""
+
 
 @app.route("/api/v1/forget", methods=["POST"])
 def forget_password() -> tuple[Response, int]:
@@ -69,7 +74,7 @@ def forget_password() -> tuple[Response, int]:
         # Generate token
         token = generate_jwt(SECRET_KEY,
                              30,
-                             {"topic": "reset-password", 
+                             {"topic": "reset-password",
                               "user_id": str(result[0]["_id"])})
         email_server = MailServer()
         message_body = load_reset_template()
@@ -80,7 +85,7 @@ def forget_password() -> tuple[Response, int]:
         message_body = message_body.replace("__LINK__", reset_url_link)
         message = Message("Reset email password", user_email, message_body)
         email_server.send(message.get_message())
-        return jsonify({"message": "We have send reset password link to your email","code": 200}), 200
+        return jsonify({"message": "We have send reset password link to your email", "code": 200}), 200
 
 
 @app.route("/api/v1/signin", methods=["POST"])
@@ -89,11 +94,13 @@ def check_user() -> dict:
     body = json.loads(request.data.decode())
     user_email = body["email"]
     user_password = body["password"]
-    result = database_obj.getRecords({"email": user_email, "password": user_password}, "Accounts")
+    result = database_obj.getRecords(
+        {"email": user_email, "password": user_password}, "Accounts")
     if len(result) == 0:
         return jsonify({"message": "Invalid credentials", "code": 400}), 400
-    thirty_days_in_min = 30*24*60 
-    token = generate_jwt(SECRET_KEY, thirty_days_in_min, {"user_id": str(result[0]["_id"])})
+    thirty_days_in_min = 30*24*60
+    token = generate_jwt(SECRET_KEY, thirty_days_in_min, {
+                         "user_id": str(result[0]["_id"])})
     return jsonify({"message": "user authenticated successfully", "token": token, "user_id": str(result[0]["_id"]), "code": 200}), 200
 
 
@@ -124,21 +131,34 @@ def signup():
         # Send confirmation email to email address
         email_server = MailServer()
         message_body = load_confirmation_template()
-        message_body = message_body.replace("__FIRST_NAME__", body["firstName"])
+        message_body = message_body.replace(
+            "__FIRST_NAME__", body["firstName"])
         token = generate_jwt(SECRET_KEY, 10, {"topic": "confirmation",
-                                             "account_id": str(result),
-                                             "email": body["email"]})
+                                              "account_id": str(result),
+                                              "email": body["email"]})
         conf_link = "http://localhost:3000/confirmation/" + token
         message_body = message_body.replace("__CONFIRMATION_LINK__", conf_link)
         message = Message("Confirmation Email", body["email"], message_body)
         # email_server.send(message.get_message())
-        t1 = threading.Thread(target=email_server.send, args=(message.get_message(),))
+        t1 = threading.Thread(target=email_server.send,
+                              args=(message.get_message(),))
         t1.start()
         return jsonify({"message": "Account created successfully and mail sent",
-                    "account_id": str(result),
-                    "code": 201}), 201
+                        "account_id": str(result),
+                        "code": 201}), 201
     except Exception as error:
         return jsonify({"message": "Duplicated email or username", "code": 400}), 400
+
+
+@app.route("/api/v1/account/<id>", methods=["PUT"])
+@token_required
+def update_account(id):
+    body = json.loads(request.data.decode())
+    try:
+        database_obj.updateRecord(ObjectId(id), body, "Accounts")
+        return jsonify({"message": "Account updated successfully"}), 200
+    except Exception as error:
+        return jsonify({"message": str(error), "code": 400}), 400
 
 
 @app.route("/api/v1/confirmation/gen", methods=["GET"])
@@ -149,11 +169,11 @@ def generate_confirmation_token():
     email = record[0]["email"]
     if record[0]["is_confirmed"] == False:
         token = generate_jwt(SECRET_KEY, 10, {"topic": "confirmation",
-                                            "account_id": account_id,
-                                            "email": email})
+                                              "account_id": account_id,
+                                              "email": email})
         # TODO: Change this link to frontend link.
         conf_link = "http://localhost:3000/confirmation/" + token
-        return jsonify({"message": "Account is already confirmed", 
+        return jsonify({"message": "Account is already confirmed",
                         "confirmation_link": conf_link, "code": 200}), 200
     else:
         return jsonify({"message": "Account is already confirmed", "code": 400}), 400
@@ -166,23 +186,24 @@ def confirmation(token):
         account_id = result["account_id"]
         topic = result["topic"]
         if topic == "confirmation":
-            account_record = database_obj.getRecordById(ObjectId(account_id), "Accounts")
+            account_record = database_obj.getRecordById(
+                ObjectId(account_id), "Accounts")
             confirmation_status = account_record["is_confirmed"]
             if confirmation_status:
-                return jsonify({"message": "This Account is already confirmed","code": 400}), 400
+                return jsonify({"message": "This Account is already confirmed", "code": 400}), 400
             else:
-                database_obj.updateRecord(ObjectId(account_id), {"is_confirmed": True}, "Accounts")
-                return jsonify({"message": "Account confirmed successfully","code": 200}), 200    
+                database_obj.updateRecord(ObjectId(account_id), {
+                                          "is_confirmed": True}, "Accounts")
+                return jsonify({"message": "Account confirmed successfully", "code": 200}), 200
     except Exception as error:
-        return jsonify({"message": "Token expired!","code": 400}), 400
-    
+        return jsonify({"message": "Token expired!", "code": 400}), 400
 
 
 @app.route("/api/v1/room", methods=["POST"])
 @token_required
 def create_room():
     body = json.loads(request.data.decode())
-    
+
     try:
         new_room = {
             "invitation_link": "",
@@ -192,7 +213,7 @@ def create_room():
             "permissions": {},
             "room_admin": body["room_admin"],
             "status": "",
-            "chat_channel": "" , # TODO: generating Chat Room record ad append its ID
+            "chat_channel": "",  # TODO: generating Chat Room record ad append its ID
             "creation_time": str(current_time())
         }
         result = database_obj.createRecord(new_room, "Rooms")
@@ -204,14 +225,14 @@ def create_room():
 @app.route("/api/v1/room/<room_id>", methods=["GET"])
 @token_required
 def get_room(room_id):
-    try:  
+    try:
         result = database_obj.getRecords({"_id": ObjectId(room_id)}, "Rooms")
         if len(result) == 0:
             return jsonify({"message": "Invalid room ID", "code": 400}), 400
         result[0]["_id"] = str(result[0]["_id"])
         return jsonify({"message": "room retrieved successfully", "data": result, "code": 200}), 200
     except Exception as error:
-        return jsonify({"message":str(error), "code": 400}), 400
+        return jsonify({"message": str(error), "code": 400}), 400
 
 
 @app.route("/api/v1/room/<room_id>", methods=["PUT"])
@@ -221,10 +242,10 @@ def update_room(room_id):
         body = json.loads(request.data.decode())
         result = database_obj.updateRecord(ObjectId(room_id), body, "Rooms")
         if result != -1:
-            return jsonify({"message": "room updated successfully", "code": 200}), 200 
+            return jsonify({"message": "room updated successfully", "code": 200}), 200
         return jsonify({"message": "Invalid room ID", "code": 400}), 400
     except Exception as error:
-        return jsonify({"message":str(error), "code": 400}), 400
+        return jsonify({"message": str(error), "code": 400}), 400
 
 
 @app.route("/api/v1/room/<room_id>", methods=["DELETE"])
@@ -233,10 +254,10 @@ def delete_room(room_id):
     try:
         result = database_obj.deleteRecordById(ObjectId(room_id), "Rooms")
         if result is None:
-            return jsonify({"message": "Invalid room ID", "code": 400}), 400       
-        return jsonify({"message": "room deleted successfully", "code": 200}), 200 
+            return jsonify({"message": "Invalid room ID", "code": 400}), 400
+        return jsonify({"message": "room deleted successfully", "code": 200}), 200
     except Exception as error:
-        return jsonify({"message":str(error), "code": 400}), 400
+        return jsonify({"message": str(error), "code": 400}), 400
 
 
 if __name__ == '__main__':
